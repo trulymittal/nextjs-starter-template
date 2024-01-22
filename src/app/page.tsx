149319e980d5672acef2c3030b5c6410849ee06b
Truly Mittal <trulymittal@gmail.com>
2024-01-22 17:08:09 +0530

    added husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ModeToggle } from '@/components/mode-toggle'
-
 export default function Home() {
+  const data = '123'
+  console.log('123')
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Hello world</h1>
